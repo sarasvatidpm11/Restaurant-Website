2784bef9245c9c6a26f3a114e9eb8880696c69ab
Saraswati <sarasvatidpm11@gmail.com>
2023-12-14 16:57:15 +0800

    navbar fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,95 +1,135 @@
 //Navbar Fixed
 
 window.onscroll = function () {
-    const header = document.querySelector("header");
-    const fixedNav = header.offsetTop;
-    const hamburgerLines = document.querySelectorAll(".hamburger-line");
-    const navmenu = document.getElementById("nav-menu");
-    const navlinks = document.querySelectorAll(".nav-link");
-  
-    if (window.scrollY > fixedNav) {
-      header.classList.add("navbar-fixed");
-      hamburgerLines.forEach((line) => {
-        line.style.backgroundColor = "black"; // Ubah warna garis hamburger menjadi hitam
-      });
-      // navmenu.style.color = "!text-black"; // Mengubah warna latar belakang elemen dengan ID tertentu menjadi hitam
-  
-      navlinks.forEach((navlink) => {
-        navlink.classList.remove("lg:text-white");
-        navlink.classList.add("lg:text-black");
-      });
-      // Ganti sumber gambar logo
-      logo.src = "./img/Logo fix hitam.png"; // Ganti dengan lokasi yang benar dari folder img
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const hamburgerLines = document.querySelectorAll(".hamburger-line");
+  const navmenu = document.getElementById("nav-menu");
+  const navlinks = document.querySelectorAll(".nav-link");
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+    header.classList.add("h-[5rem]");
+    hamburgerLines.forEach((line) => {
+      line.style.backgroundColor = "black"; // Ubah warna garis hamburger menjadi hitam
+    });
+    // navmenu.style.color = "!text-black"; // Mengubah warna latar belakang elemen dengan ID tertentu menjadi hitam
+
+    navlinks.forEach((navlink) => {
+      navlink.classList.remove("lg:text-white");
+      navlink.classList.add("lg:text-black");
+    });
+    // Ganti sumber gambar logo
+    logo.src = "./img/Logo fix hitam.png"; // Ganti dengan lokasi yang benar dari folder img
+  } else {
+    header.classList.remove("navbar-fixed");
+    header.classList.remove("h-[5rem]");
+    hamburgerLines.forEach((line) => {
+      line.style.backgroundColor = "white"; // Ubah warna garis hamburger menjadi putih (default)
+    });
+    // navmenu.style.color = "white"; // Mengubah warna latar belakang elemen dengan ID tertentu menjadi putih (default)
+
+    navlinks.forEach((navlink) => {
+      navlink.classList.add("lg:text-white");
+      navlink.classList.remove("lg:text-black");
+    });
+
+    // Kembalikan sumber gambar logo ke semula
+    logo.src = "./img/Logo fix.png";
+  }
+};
+
+// Rubah warna sub menu navbar
+document.addEventListener("DOMContentLoaded", function () {
+  var currentPage = window.location.pathname;
+  var navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(function (link) {
+    var linkHref = link.getAttribute("href");
+    currentPage = currentPage.replace(/\/$/, "");
+
+    console.log(linkHref, currentPage);
+    if (linkHref === currentPage) {
+      link.closest("a").classList.add("navlinkactive");
+    }
+  });
+});
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//Accordion
+const accordionHeader = document.querySelectorAll(".accordion-header");
+
+accordionHeader.forEach((accordionHeader) => {
+  accordionHeader.addEventListener("click", (event) => {
+    accordionHeader.classList.toggle("active");
+    const accordionBody = accordionHeader.nextElementSibling;
+    if (accordionHeader.classList.contains("active")) {
+      accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
     } else {
-      header.classList.remove("navbar-fixed");
-      hamburgerLines.forEach((line) => {
-        line.style.backgroundColor = "white"; // Ubah warna garis hamburger menjadi putih (default)
-      });
-      navmenu.style.color = "white"; // Mengubah warna latar belakang elemen dengan ID tertentu menjadi putih (default)
-  
-      navlinks.forEach((navlink) => {
-        navlink.classList.add("lg:text-white");
-        navlink.classList.remove("lg:text-black");
-      });
-  
-      // Kembalikan sumber gambar logo ke semula
-      logo.src = "./img/Logo fix.png";
+      accordionBody.style.maxHeight = 0;
     }
+  });
+});
+
+//Gallery
+document.querySelectorAll(".image-container-gallery img").forEach((image) => {
+  image.onclick = () => {
+    document.querySelector(".popup-image-gallery").style.display = "block";
+    document.querySelector(".popup-image-gallery img").src =
+      image.getAttribute("src");
   };
-  
-  // Rubah warna sub menu navbar
-  // document.addEventListener("DOMContentLoaded", function () {
-  //   var currentPage = window.location.pathname;
-  //   var navLinks = document.querySelectorAll(".nav-link");
-  
-  //   navLinks.forEach(function (link) {
-  //     var linkHref = link.getAttribute("href");
-  //     currentPage = currentPage.replace(/\/$/, "");
-  
-  //     if (linkHref === currentPage) {
-  //       link.closest("a").classList.add("active");
-  //     }
-  //   });
-  // });
+});
 
-  // Hamburger
-  const hamburger = document.querySelector("#hamburger");
-  const navMenu = document.querySelector("#nav-menu");
-  
-  hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("hamburger-active");
-    navMenu.classList.toggle("hidden");
+document.querySelector(".popup-image-gallery span").onclick = () => {
+  document.querySelector(".popup-image-gallery").style.display = "none";
+};
+
+function openTab(tabName) {
+  const tabs = document.querySelectorAll('.tab-content');
+  const tabButtons = document.querySelectorAll('button');
+
+  tabs.forEach(tab => {
+    tab.classList.add('hidden');
   });
 
-  //Accordion
-  const accordionHeader = document.querySelectorAll('.accordion-header');
+  tabButtons.forEach(btn => {
+    btn.classList.remove('active-tab');
+  });
 
-  accordionHeader.forEach(accordionHeader => {
-    accordionHeader.addEventListener("click", event => {
-      accordionHeader.classList.toggle("active")
-      const accordionBody = accordionHeader.nextElementSibling
-      if(accordionHeader.classList.contains("active")) {
-        accordionBody.style.maxHeight = accordionBody.scrollHeight + "px"
-      }
-      else {
-        accordionBody.style.maxHeight = 0
-      }
-    })
-  })
+  document.getElementById(tabName).classList.remove('hidden');
+  document.getElementById('tabBtn' + tabName.slice(-1)).classList.add('active-tab');
+}
+
+// Set Tab 1 as active by default
+document.getElementById('tab1').classList.remove('hidden');
+document.getElementById('tabBtn1').classList.add('active-tab');
 
 
-  //Gallery
-  document.querySelectorAll('.image-container-gallery img').forEach(image =>{
-    image.onclick = () =>{
-      document.querySelector('.popup-image-gallery').style.display = 'block';
-      document.querySelector('.popup-image-gallery img').src = image.getAttribute('src');
+
+
+
+
+let index = 0;
+  const images = document.querySelectorAll('#slider > div');
+
+  function showImage() {
+    for (let i = 0; i < images.length; i++) {
+      images[i].classList.add('hidden');
     }
-  });
-
-  document.querySelector('.popup-image-gallery span').onclick = () =>{
-    document.querySelector('.popup-image-gallery').style.display = 'none';
+    images[index].classList.remove('hidden');
+    index = (index + 1) % images.length;
   }
 
-  
-  
-  
+  function startSlider() {
+    setInterval(showImage, 3000); // Ganti gambar setiap 3 detik
+  }
+
+  startSlider();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/*.{html,js}"
-  ],
+  content: ["./public/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        primary:'#7C0A02', //merah dark
-        secondary: '#B22222', //merah light
-        tertiary: '#F1BC31', //kuning
-        quaternary: '#242121', //hitam
+        primary: "#7C0A02", //merah dark
+        secondary: "#B22222", //merah light
+        tertiary: "#F1BC31", //kuning
+        quaternary: "#242121", //hitam
+        last: "#8D8887", //abu
       },
       fontFamily: {
-        'philosopher': ['Philosopher', 'sans-serif'],
-        'port-lligat-sans': ['Port Lligat Sans', 'sans-serif'],
-        'inspiration': ['Inspiration', 'cursive'],
+        philosopher: ["Philosopher", "sans-serif"],
+        "port-lligat-sans": ["Port Lligat Sans", "sans-serif"],
+        inspiration: ["Inspiration", "cursive"],
       },
     },
   },
   plugins: [],
-}
-
+};
